919efe92e642e6c1e9fdd6c17bedaafe8db39b30
Yamil Díaz Aguirre <Yamil.Diaz-non-empl@moodys.com>
2024-03-13 14:13:23 -0600

    calculate hash

--- brew_formula.rb
+++ brew_formula.rb
@@ -1,7 +1,7 @@
 class BrewFormula < Formula
   desc "Guide you through a breathing exercise with customizable iterations and patterns"
   homepage "https://github.com/Yamilquery/homebrew-breathe"
-  url "https://github.com/Yamilquery/homebrew-breathe/archive/v1.0.0.tar.gz"
+  url "https://github.com/Yamilquery/homebrew-breathe/archive/v1.0.1.tar.gz"
   sha256 "a8cce4d704b722010566afe18a59bc5ab0a9e841da84cc20fd72aacf10a4bf46"
   license "MIT"
 
